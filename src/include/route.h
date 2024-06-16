107f12030cfb73d31fc0a6c7a316507d762ed3ba
nhermann <161588164+nadiahermann111@users.noreply.github.com>
2024-06-16 14:13:03 +0200

    Update route.h

--- src/include/route.h
+++ src/include/route.h
@@ -1,32 +1,3 @@
-/*#ifndef ROUTE_H
-#define ROUTE_H
-
-#include <vector>
-
-/*class Route {
-public:
-    Route(const std::vector<int>& brightnessLevels, int maxSteps);
-    int calculateStops();
-    int calculateListens();
-    static int findOptimalStartPoint(const std::vector<int>& brightnessLevels, int maxSteps);
-    static int bestPoint(const std::vector<int> &brightnessLevels, int &energy, int start);
-
-
-private:
-    std::vector<int> brightness;
-    int max_steps;
-
-    int calculateStops(int guardEnergy);
-};
-
-
-
-
-
-#endif // ROUTE_H*/
-
-
-
 #ifndef ROUTE_H
 #define ROUTE_H
 
@@ -36,7 +7,6 @@ class Route {
 public:
     Route(const std::vector<int>& brightnessLevels, int maxSteps);
     std::pair<int, int> calculateStops(int start);
-    int calculateListens();
 
 
 private:
